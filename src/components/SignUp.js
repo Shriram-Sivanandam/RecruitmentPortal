1589c611c442753f9f329248b7869a253237f1e1
Shriram-Sivanandam <ssshriram4@gmail.com>
2021-10-12 12:32:07 +0530

    auto login on singup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,12 +3,14 @@ import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 // import "./Signup.css";
 import { db } from "../firebase";
+import { auth } from "../firebase";
+import userEvent from "@testing-library/user-event";
 
 export default function SignUp() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
-  const { signup } = useAuth();
+  const { currentUser, signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
@@ -36,7 +38,6 @@ export default function SignUp() {
     } catch {
       setError("Failed to create an account");
     }
-
     setLoading(false);
   }
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { auth } from "../firebase";
+import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 
 const AuthContext = React.createContext();
 
@@ -10,10 +11,35 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
+  const auth1 = getAuth();
+  const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: "http://localhost:3000/",
+    handleCodeInApp: true,
+  };
 
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password);
-  }
+  // function signup(email, password) {
+  //   return auth.createUserWithEmailAndPassword(email, password);
+  // }
+  const signup = (email, password) => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // userCredential.user.sendEmailVerification();
+        sendSignInLinkToEmail(auth1, email, actionCodeSettings)
+          .then(() => {
+            window.localStorage.setItem("emailForSignIn", email);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+          });
+        // auth.signOut();
+        alert("Email sent for verification");
+      })
+      .catch(alert);
+  };
 
   function login(email, password) {
     return auth.signInWithEmailAndPassword(email, password);
