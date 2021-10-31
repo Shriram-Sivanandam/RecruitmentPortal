afdcc24eb8010a95ae5477493c81317c76d9dc56
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-10-31 11:34:07 +0000

    added config

--- src/App.js
+++ src/App.js
@@ -12,11 +12,8 @@ import AdminDashboard from "./components/admin/Dashboard/AdminDashboard";
 import Quizbox from "./components/Dashboard/Quizbox";
 import QuesUpload from "./components/admin/Dashboard/UploadQuestions";
 import StudentList from "./components/StudentList/StudentList";
-<<<<<<< HEAD
 import Landing1 from "./components/Landing/Landing1";
-=======
 import StickerWall from "./components/StickerWall/StickerWall";
->>>>>>> 7bab9352935194dcf1ccb60a1c8fcff6bc343ddb
 
 function App() {
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCjLjOs1llvTVoAYy-6ahQcvcT-PPj58A0",
+  authDomain: "stcrecruitmentportal.firebaseapp.com",
+  projectId: "stcrecruitmentportal",
+  storageBucket: "stcrecruitmentportal.appspot.com",
+  messagingSenderId: "623361095597",
+  appId: "1:623361095597:web:3baf37d46d40b6c1bad324",
+  measurementId: "G-SCY49WP6KJ"
 });
 
 var db = app.firestore();
