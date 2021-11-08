a8778431f892f92c5558b37b6c8e5e98889184c9
Shriram-Sivanandam <ssshriram4@gmail.com>
2021-11-08 20:23:23 +0530

    horizontal scroll

--- src/components/Enough/Enough.js
+++ src/components/Enough/Enough.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Enough.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import enoughSent from "../../assets/enoughSent.svg";
+import nowHand from "../../assets/nowHand.png";
 import HorizontalScroll from "react-scroll-horizontal";
 // import { Element } from "react-scroll";
 import { useHorizontalScroll } from "../useHorizontalScroll";
 
 function Enough() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, delay: 3000 });
+  }, []);
   const scrollRef = useHorizontalScroll();
   return (
     <div className="enoughCont">
       <div ref={scrollRef} className="imgCont">
+        <div data-aos="fade-down">
+          <img className="enoughHand" src={nowHand}></img>
+        </div>
         <img className="enoughImg" src={enoughSent}></img>
       </div>
     </div>
